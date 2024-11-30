c2d2c0f7cd4d71e0972ad97cce06846130367fd4
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-11-30 04:05:39 -0600

    Fixed spawn of 2 circles on start as the first client joins

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -62,12 +62,13 @@ public class GameLogic : MonoBehaviour
         return avatar;
     }
 
-
-
-    public void UpdateAvatarPosition(GameObject avatar, Vector2 positionInPercent)
+    public void UpdateAvatarPosition(GameObject avatar, Vector2 newPosition)
     {
-        Vector3 worldPos = ConvertToWorldPosition(positionInPercent);
-        avatar.transform.position = Vector3.Lerp(avatar.transform.position, worldPos, Time.deltaTime * 10f);
+        Vector3 targetPosition = ConvertToWorldPosition(newPosition);
+        if (avatar != null)
+        {
+            avatar.transform.position = Vector3.Lerp(avatar.transform.position, targetPosition, Time.deltaTime * 10);
+        }
     }
 
     private Vector3 ConvertToWorldPosition(Vector2 percentPosition)
